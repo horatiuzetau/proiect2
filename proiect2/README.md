0cbaeec3d3c25f76658b5156a26cda167b78a0d8
horatiuzetau <horatiu.atanasoaei16@gmail.com>
2020-01-05 17:36:25 +0200

    Base structure for the platform.

---
+++ proiect2/README.md
@@ -0,0 +1,5 @@
+# proiect2
+ASP.NET C# Web Application (Project for faculty)
+
+
+## This branch represents the base structure of the project.
---
+++ proiect2/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using proiect2.Models;
+
+[assembly: OwinStartupAttribute(typeof(proiect2.Startup))]
+namespace proiect2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            createAdminUserAndApplicationRoles();
+        }
+        private void createAdminUserAndApplicationRoles()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            // Se adauga rolurile aplicatiei
+           
+            if (!roleManager.RoleExists("User"))
+            {
+                var role = new IdentityRole();
+                role.Name = "User";
+                roleManager.Create(role);
+            }
+        }
+    }
+}
